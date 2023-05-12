725c7cb116ea8d0c431a0cb0b1afa600efffbf99
ekrasmussen <emil@erasmussen.dk>
2023-05-12 23:18:56 +0200

    added users route

---
+++ Auth-API/routes/users-routes.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const db = require('../db.js')
+const bcrypt = require('bcrypt');
+
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    try{
+        const users = await db.pool.query("SELECT * FROM users");
+        res.json({users : users.rows});
+    } catch(error) {
+        res.status(500).json({error:error.message});
+    }
+})
+
+router.post('/', async (req, res) => {
+    try{
+        const hashedPassword = await bcrypt.hash(req.body.password,10);
+        const newUser = await db.pool.query('INSERT INTO users (username, email, hashedPassword, perms) VALUES ($1, $2, $3, $4) RETURNING *', 
+        [req.body.username,req.body.email,hashedPassword,"User"]);
+        res.json({user: newUser});
+    } catch (error) {
+        res.status(500).json({error:error.message});
+    }
+})
+
+module.exports = { router };
