ed7a2bb1281623dbdbfc8bd543f17477b11e7be9
ekrasmussen <emil@erasmussen.dk>
2023-05-12 23:58:09 +0200

    created refresh token route

--- Auth-API/routes/auth-routes.js
+++ Auth-API/routes/auth-routes.js
@@ -2,8 +2,10 @@ const express = require('express');
 const db = require('../db.js');
 const bcrypt = require('bcrypt');
 const tokens = require('../utils/jwt-helpers.js');
-
+const jwt = require('jsonwebtoken');
 const router = express.Router();
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, "../.env")})
 
 router.post('/login', async (req, res) => {
     try{
@@ -21,6 +23,21 @@ router.post('/login', async (req, res) => {
         res.cookie('refresh_token', jwtTokens.refreshToken, {httpOnly: true});
         res.json(jwtTokens);
 
+    } catch(error) {
+        res.status(500).json({error:error.message});
+    }
+})
+
+router.get('/refresh_token', (req, res) => {
+    try {
+        const refreshToken = req.cookies.refresh_token;
+        if(refresh_token === null) return res.status(401).json({error: 'Null refresh token'});
+        jwt.verify(refreshToken, process.env.AUTH_REFRESH_TOKEN, (error, user) => {
+            if(error) return res.status(403).json({error:error.message});
+            let newTokens = tokens.jwtTokens(user);
+            res.cookie('refresh_token', newTokens.refreshToken, { httpOnly: true});
+            res.json(newTokens);
+        });
     } catch(error) {
         res.status(500).json({error:error.message});
     }
